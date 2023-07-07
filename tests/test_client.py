09c6cb4c77daff74a857feedbd024ad6110b6411
LloydTao <LL23@live.co.uk>
2023-07-07 08:49:43 +0100

    Remove async

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,12 +1,9 @@
-import pytest
-
 from bullhorn import __version__
 from bullhorn.client import BullhornClient
 from bullhorn.types import ping
 
 
-@pytest.mark.asyncio
-async def test_bullhorn_client_ping(mocker):
+def test_bullhorn_client_ping(mocker):
     # Mock response
     return_value: ping.Ping = {"sessionExpires": 1234567891011}
     mocker.patch(
@@ -19,7 +16,7 @@ async def test_bullhorn_client_ping(mocker):
         rest_url="https://rest123.bullhornstaffing.com/rest-services/1234/",
     )
     # Get result
-    result = await bc.ping()
+    result = bc.ping()
     assert "sessionExpires" in result
 
 
