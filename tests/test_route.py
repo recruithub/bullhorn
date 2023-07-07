382c3e495bf9320646e8a0fab3c6aacd037cb4b4
LloydTao <LL23@live.co.uk>
2023-07-07 07:25:27 +0100

    Add Route class unit tests

---
+++ tests/test_route.py
@@ -0,0 +1,41 @@
+from bullhorn.route import Route
+
+
+def test_route_url():
+    method = "GET"
+    path = "https://rest123.bullhornstaffing.com/rest-services/1234/" + "ping"
+    path_params = {}
+    query_params = {}
+    route = Route(
+        method,
+        path,
+        path_params,
+        query_params,
+    )
+    assert route.url == "https://rest123.bullhornstaffing.com/rest-services/1234/ping"
+
+
+def test_route_with_params_url():
+    method = "GET"
+    path = (
+        "https://rest123.bullhornstaffing.com/rest-services/1234/"
+        + "entity/{entityType}/{entityId}"
+    )
+    path_params = {
+        "entityType": "Candidate",
+        "entityId": "123456789",
+    }
+    query_params = {
+        "fields": "firstName,lastName,address",
+        "count": 5,
+    }
+    route = Route(
+        method,
+        path,
+        path_params,
+        query_params,
+    )
+    assert (
+        route.url
+        == "https://rest123.bullhornstaffing.com/rest-services/1234/entity/Candidate/123456789?fields=firstName%2ClastName%2Caddress&count=5"
+    )
