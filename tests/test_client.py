64018e543edb1ae4e8b9fe13feaf85d13c168efa
LloydTao <LL23@live.co.uk>
2023-07-21 16:41:26 +0100

    Add get_client_contacts unit test

--- tests/test_client.py
+++ tests/test_client.py
@@ -4,6 +4,7 @@ from bullhorn import __version__
 from bullhorn.client import BullhornClient
 from bullhorn.types import (
     candidate,
+    client_contact,
     ping,
 )
 
@@ -61,3 +62,30 @@ def test_client_get_candidates(mocker):
         fields="id,firstName,middleName,lastName",
     )
     assert "firstName" in result[0]
+
+
+def test_client_get_client_contacts(mocker):
+    # Mock response
+    return_value: List[client_contact.ClientContact] = [
+        {
+            "id": 1234567891011,
+            "firstName": "Example",
+            "middleName": "C.",
+            "lastName": "Name",
+        },
+    ]
+    mocker.patch(
+        "bullhorn.client.BullhornClient.request",
+        return_value=return_value,
+    )
+    # Initialise client
+    bc = BullhornClient(
+        token="12345_1234567_a12345bc-123a-45bc-67de-12345678910a",
+        rest_url="https://rest123.bullhornstaffing.com/rest-services/1234/",
+    )
+    # Get result
+    result = bc.get_client_contacts(
+        query="dateLastModified:{2023/01/01 TO *}",
+        fields="id,firstName,middleName,lastName",
+    )
+    assert "lastName" in result[0]
