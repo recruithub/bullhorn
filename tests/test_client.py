aa6e68ec7dc983109a098ddc112a8de8c473c75e
LloydTao <LL23@live.co.uk>
2023-07-21 16:41:13 +0100

    Add get_candidates unit test

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,6 +1,11 @@
+from typing import List
+
 from bullhorn import __version__
 from bullhorn.client import BullhornClient
-from bullhorn.types import ping
+from bullhorn.types import (
+    candidate,
+    ping,
+)
 
 
 def test_client_user_agent():
@@ -29,3 +34,30 @@ def test_client_ping(mocker):
     # Get result
     result = bc.ping()
     assert "sessionExpires" in result
+
+
+def test_client_get_candidates(mocker):
+    # Mock response
+    return_value: List[candidate.Candidate] = [
+        {
+            "id": 1234567891011,
+            "firstName": "Example",
+            "middleName": "C.",
+            "lastName": "Name",
+        },
+    ]
+    mocker.patch(
+        "bullhorn.client.BullhornClient.request",
+        return_value=return_value,
+    )
+    # Initialise client
+    bc = BullhornClient(
+        token="12345_1234567_a12345bc-123a-45bc-67de-12345678910a",
+        rest_url="https://rest123.bullhornstaffing.com/rest-services/1234/",
+    )
+    # Get result
+    result = bc.get_candidates(
+        query="dateLastModified:{2023/01/01 TO *}",
+        fields="id,firstName,middleName,lastName",
+    )
+    assert "firstName" in result[0]
